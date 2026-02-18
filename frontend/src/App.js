25c8c3d8c254af5bd5ecda64be7262ede6c070b8
Meet Sheth <mitsheth2@gmail.com>
2026-02-18 18:05:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import AdminDashboard from "./components/AdminPanel/AdminDashboard/AdminDashboar
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/HomeScreen/Home';
+import Login from './components/LoginScreen/Login';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path="/" element={<AdminDashboard />} />
+        <Route exact path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
