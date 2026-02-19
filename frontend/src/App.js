a9bbe81d927169a60d7f01c6419ac608edbb16f9
Meet Sheth <mitsheth2@gmail.com>
2026-02-20 00:54:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminNavbar from "./components/AdminPanel/AdminNavbar/AdminNavbar";
-import AdminSidebar from "./components/AdminPanel/AdminSidebar/AdminSidebar";
-import AdminDashboard from "./components/AdminPanel/AdminDashboard/AdminDashboard";
+import React, { useState, useEffect } from "react";
+import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+// Layout Components (SAFE IMPORTS)
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 // Website Components
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './components/HomeScreen/Home';
 import AboutUs from './components/AboutUsScreen/AboutUs';
+import ContactForm from './components/ContactUsScreen/contactUs';
+import Login from './components/LoginScreen/Login';
+import CustomerCart from './components/CustomerPanel/CustomerCart/CustomerCart';
+import Products from './components/ProductScreen/Products';
 import Categories from "./components/CategoryScreen/Category";
 import CategoryProducts from "./components/CategoryScreen/CategoryProducts";
-
-import Login from './components/LoginScreen/Login';
+import ProductDetail from './components/ProductScreen/ProductDetail';
+import AudioBook from "./components/ProductScreen/AudioBook";
+import VideoBook from "./components/ProductScreen/VideoBook";
+import E_Book from "./components/ProductScreen/E_Book";
+import Payment from "./components/PaymentScreen/Payment";
+import Invoice from "./components/CustomerPanel/CustomerOrders/Invoice";
+import Entertainment from "./components/CategoryScreen/Entertainment";
+import Technology from "./components/CategoryScreen/Technology";
 
 // Customer Panel
 import CustomerRegisteration from './components/CustomerPanel/CustomerRegistration/CustomerRegisteration';
@@ -24,6 +39,16 @@ import SendOtp from './components/CustomerPanel/CustomerForgetPassword/SendOtp';
 import ResetPassword from './components/CustomerPanel/CustomerForgetPassword/ResetPassword';
 import CustomerHelpSupport from "./components/CustomerPanel/CustomerHelpSupport/CustomerHelpSupport";
 
+// Employee Panel
+import EmployeeDashboard from './components/EmployeePanel/EmployeeDashboard/EmployeeDashboard';
+import EmployeeManageOrders from './components/EmployeePanel/EmployeeManageOrders/EmployeeManageOrders';
+import EmployeeViewCategory from './components/EmployeePanel/EmployeeViewCategory/EmployeeViewCategory';
+import EmployeeManageBookType from './components/EmployeePanel/EmployeeManageBookType/EmployeeManageBookType';
+import EmployeeManageProducts from './components/EmployeePanel/EmployeeManageProducts/EmployeeManageProducts';
+import EmployeeProfile from './components/EmployeePanel/EmployeeProfile/EmployeeProfile';
+import EmployeeForgetPassword from './components/EmployeePanel/EmployeeForgetPassword/EmployeeForgetPassword';
+import EmployeeAddBookType from './components/EmployeePanel/EmployeeManageBookType/EmployeeAddBookType';
+import EmployeeAddProducts from './components/EmployeePanel/EmployeeManageProducts/EmployeeAddProducts';
 
 // Admin Panel
 import AdminDashboard from './components/AdminPanel/AdminDashboard/AdminDashboard';
@@ -40,38 +65,99 @@ import AdminAddCategory from './components/AdminPanel/AdminManageCategory/AdminA
 import AdminAddBookType from './components/AdminPanel/AdminManageBookType/AdminAddBookType';
 import AdminAddEmployees from './components/AdminPanel/AdminManageEmployees/AdminAddEmployees';
 import AdminAddProducts from './components/AdminPanel/AdminManageProducts/AdminAddProducts';
-import Technology from "./components/CategoryScreen/Technology";
 
+// Context
+import { CartContext, UserProvider } from './Context';
+
+const checkCart = localStorage.getItem('cartData');
 
 function App() {
+
+  const [cartData, setCartData] = useState(checkCart ? JSON.parse(checkCart) : []);
+  const [products, setProducts] = useState([]);
+  const [employeeList, setEmployeeList] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [bookTypes, setBookTypes] = useState([]);
+
+  const handleAddCategory = (newCategory) => {
+    setCategories((prev) => [...prev, newCategory]);
+  };
+
+  const handleAddProduct = (newProduct) => {
+    setProducts((prev) => [...prev, newProduct]);
+  };
+
+  const handleAddBookType = (newBookType) => {
+    newBookType.Book_ID = bookTypes.length + 1;
+    setBookTypes((prev) => [...prev, newBookType]);
+  };
+
+  const handleAddEmployee = (newEmployee) => {
+    setEmployeeList((prev) => [...prev, newEmployee]);
+  };
+
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  const noHeaderFooterRoutes = [
+    '/employee/dashboard',
+    '/employee/manage-orders',
+    '/employee/manage-categories',
+    '/employee/manage-booktype',
+    '/employee/add-booktype',
+    '/employee/manage-products',
+    '/employee/add-products',
+    '/employee/profile',
+    '/employee/forget-password',
+    '/admin/dashboard',
+    '/admin/manage-orders',
+    '/admin/manage-categories',
+    '/admin/add-category',
+    '/admin/manage-booktype',
+    '/admin/add-booktype',
+    '/admin/manage-products',
+    '/admin/add-products',
+    '/admin/view-products',
+    '/admin/manage-employees',
+    '/admin/add-employees',
+    '/admin/view-customers',
+    '/admin/profile',
+    '/admin/manage-feedback',
+    '/admin/reports',
+  ];
+
+  const shouldDisplayHeaderFooter = !noHeaderFooterRoutes.includes(location.pathname);
+
   return (
-    <BrowserRouter>
-      <AdminNavbar />
-      <AdminSidebar />
+    <UserProvider>
+      <CartContext.Provider value={{ cartData, setCartData }}>
+        {shouldDisplayHeaderFooter && <Header />}
 
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path="/" element={<AdminDashboard />} />
-        <Route exact path='/login' element={<Login />} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/aboutus' element={<AboutUs />} />
+          <Route path='/contactus' element={<ContactForm />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/cart' element={<CustomerCart />} />
+          <Route path='/payment' element={<Payment />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path='/category/:category_slug/:Category_ID' element={<CategoryProducts />} />
+          <Route path="/products/Entertainment" element={<Entertainment />} />
+          <Route path="/products/Technology" element={<Technology />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/product/:product_slug/:Product_ID' element={<ProductDetail />} />
+          <Route path='/audio-book' element={<AudioBook />} />
+          <Route path='/video-book' element={<VideoBook />} />
+          <Route path='/e-book' element={<E_Book />} />
+          <Route path='/invoice' element={<Invoice />} />
+        </Routes>
 
-        
-          {/* Admin */}
-          <Route exact path='/admin/dashboard' element={<AdminDashboard />} />
-          <Route exact path='/admin/manage-orders' element={<AdminManageOrders />} />
-          <Route path="/admin/manage-categories" element={<AdminManageCategory categories={categories} onAddCategory={handleAddCategory} />} />
-          <Route path="/admin/add-category" element={<AdminAddCategory onAddCategory={handleAddCategory} />} />
-          <Route path="/admin/manage-booktype" element={<AdminManageBookType bookTypes={bookTypes} onAddBookType={handleAddBookType} />} />
-          <Route path="/admin/add-booktype" element={<AdminAddBookType onAddBookType={handleAddBookType} />} />
-          <Route exact path='/admin/manage-products' element={<AdminManageProducts products={products} />} />
-          <Route exact path='/admin/add-products' element={<AdminAddProducts onAddProduct={handleAddProduct} />} />
-          <Route path="/admin/manage-employees" element={<AdminManageEmployees employeeList={employeeList} onAddEmployee={handleAddEmployee} />} />
-          <Route path="/admin/add-employees" element={<AdminAddEmployees onAddEmployee={handleAddEmployee} />} />
-          <Route exact path='/admin/view-customers' element={<AdminViewCustomers />} />
-          <Route exact path='/admin/profile' element={<AdminProfile />} />
-          <Route exact path='/admin/manage-feedback' element={<AdminManageFeedback />} />
-          <Route exact path='/admin/reports' element={<Reports />} />
-      </Routes>
-    </BrowserRouter>
+        {shouldDisplayHeaderFooter && <Footer />}
+      </CartContext.Provider>
+    </UserProvider>
   );
 }
 
