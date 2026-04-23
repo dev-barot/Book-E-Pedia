3e72fa05c826930d6b1ad12e2462d3554f093ab6
heer00 <mehata.heer9@gmail.com>
2026-04-23 17:54:02 +0530

    fixed cloudinary import error

--- backend/bookepedia_backend/bookepedia_backend/settings.py
+++ backend/bookepedia_backend/bookepedia_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import cloudinary
 from dotenv import load_dotenv 
 load_dotenv()
 cloudinary.config(
