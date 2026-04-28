788a1f9d8a27440dc71f8b55a76734d8983464eb
Deep <deepboghara6@gmail.com>
2026-04-28 14:56:19 +0530

    fix bug

--- backend/bookepedia_backend/api/views.py
+++ backend/bookepedia_backend/api/views.py
@@ -1265,8 +1265,6 @@ def create_order(request):
                 total_amount += amount
                 total_quantity += qty
             master_order.T_Amount = total_amount * Decimal("1.12")
-
-            master_order.T_Amount = total_amount * 1.12
             master_order.T_Quantity = total_quantity
             master_order.save()
 
